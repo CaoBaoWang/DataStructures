44f8d809aff5c95f8304a8b3c01762a9d96eba4e
汪少立 <1125550225@qq.com>
2018-03-12 14:41:36 +0800

    4.2

--- DataStructures_Algorithms_in_java/src/main/java/com/hyd/smart/mylibrary/D4/Quene/queue.java
+++ DataStructures_Algorithms_in_java/src/main/java/com/hyd/smart/mylibrary/D4/Quene/queue.java
@@ -42,6 +42,18 @@ class Queue
       {
       return queArray[front];
       }
+
+    public void display(){
+      int i = rear;
+      while (true){
+         long l= peekFront();
+         System.out.print(peekFront()+" ");
+         remove();
+         if(l==queArray[i])break;
+      }
+      System.out.println();
+
+    }
 //--------------------------------------------------------------
    public boolean isEmpty()    // true if queue is empty
       {
@@ -59,6 +71,81 @@ class Queue
       }
 //--------------------------------------------------------------
    }  // end class Queue
+
+class Deque{
+   private int maxSize;
+   private long[] dequeArray;
+   private int front;
+   private int rear;
+   private int nItems;
+
+   public Deque(int maxSize){
+      this.maxSize=maxSize;
+      dequeArray=new long[maxSize];
+      front=1;
+      rear=0;
+      nItems=0;
+   }
+
+
+   public void insertLeft(long i){
+      if(front==0){
+         front=maxSize;
+      }
+      dequeArray[--front]=i;
+
+      nItems++;
+   }
+   public void insertRight(long i){
+      if(rear==maxSize-1){
+         rear=-1;
+      }
+      dequeArray[++rear]=i;
+      nItems++;
+
+   }
+   public long  removeLeft(){
+      long temp = dequeArray[front++];
+      if(front==maxSize){
+         front=0;
+      }
+      return  temp;
+   }
+   public long removeRight(){
+      long temp = dequeArray[rear--];
+      if(rear==-1){
+         rear=maxSize-1;
+      }
+      return temp;
+   }
+   public boolean isFull(){
+      return  nItems==maxSize;
+   }
+   public boolean isEmpty(){
+      return  nItems ==0;
+   }
+
+   public void display(){
+
+      int i = front;
+      while (true){
+         System.out.print(dequeArray[i]+" ");
+         if(i==rear)break;
+         i ++;
+         if(i==maxSize) i=0;
+
+      }
+      System.out.println();
+   }
+   public void displayArr(){
+      for(long i:dequeArray)
+         System.out.print(i+" ");
+         System.out.println();
+
+   }
+}
+
+
 ////////////////////////////////////////////////////////////////
 class QueueApp
    {
@@ -87,6 +174,25 @@ class QueueApp
          System.out.print(" ");
          }
       System.out.println("");
+         theQueue.display();
+         theQueue.display();
+         Deque deque=new Deque(4);
+         deque.insertLeft(10);
+         deque.insertRight(20);
+         deque.insertRight(30);
+         deque.insertLeft(40);
+//         deque.displayArr();
+         deque.display();
+         deque.removeLeft();
+         deque.removeRight();
+         deque.display();
+         deque.insertRight(30);
+         deque.insertRight(40);
+         deque.display();
+         deque.removeRight();
+         deque.removeRight();
+         deque.removeRight();
+         deque.display();
       }  // end main()
    }  // end class QueueApp
 ////////////////////////////////////////////////////////////////
